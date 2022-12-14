33ea957f71edc2a1ea0511c4c353a845be3ce062
Khant <58853442+khant21699@users.noreply.github.com>
2022-12-14 19:12:41 +0630

    Portfolio

---
+++ script.js
@@ -0,0 +1,5 @@
+const toggleNav = () => {
+  document.querySelector(".overlay").classList.toggle("active");
+  document.querySelector(".navlinks").classList.toggle("active");
+  document.querySelector(".toggle").classList.toggle("active");
+};
