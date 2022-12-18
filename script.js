e512aacacf049df92609219390277b860d1d4206
Khant <58853442+khant21699@users.noreply.github.com>
2022-12-18 19:35:34 +0630

    Portfolio

--- script.js
+++ script.js
@@ -3,3 +3,18 @@ const toggleNav = () => {
   document.querySelector(".navlinks").classList.toggle("active");
   document.querySelector(".toggle").classList.toggle("active");
 };
+
+const hidden = document.querySelectorAll(".hidden");
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
+
+hidden.forEach((el) => {
+  observer.observe(el);
+});
